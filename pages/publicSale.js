ff24222bf9846f47ab9cbc8e42ce838309bfa71c
Spider-Man by day <sadiqherritage@gmail.com>
2025-03-08 08:23:45 +0100

    🛠 attempt fix cross-function

--- pages/publicSale.js
+++ pages/publicSale.js
@@ -139,7 +139,7 @@ export default function PublicSale() {
   async function connectWallet() {
     try {
       if (!window.ethereum) {
-        setErrorMessage("No Ethereum wallet detected. Please install MetaMask.");
+        setErrorMessage("Please open this site in MetaMask browser or use WalletConnect");
         return;
       }
       
@@ -296,7 +296,7 @@ export default function PublicSale() {
               objectFit="responsive"
               className="object-cover"
             />
-            <div className="flex absolute w-[75px] h-[30px] pt-[3px] top-3 left-3 justify-center items-center px-2 bg-[#0c4218] border border-[#5cbb5c]/30 rounded-md text-white text-sm z-10">
+            <div className="flex absolute w-[75px] h-[30px] pt-[3px] top-3 md:left-3 left-7 justify-center items-center px-2 bg-[#0c4218] border border-[#5cbb5c]/30 rounded-md text-white text-sm z-10">
             {totalSupply} <span className="text-[#5cbb5c]"> /{maxSupply}</span>
             </div>
           </div>
